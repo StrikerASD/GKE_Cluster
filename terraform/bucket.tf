c48e6637eac9da2d2e2720efc773d6cad20128c3
izaleski <iwo.zaleski@payback.net>
2024-07-08 10:57:20 +0200

    Removed output usage

--- terraform/bucket.tf
+++ terraform/bucket.tf
@@ -2,7 +2,7 @@ module "gke_state_bucket" {
   source = "github.com/StrikerASD/GKE_Module_State_Bucket"
   google_kms_key_ring_project = "sb-izal-20240703-111957"
   google_kms_crypto_key_key_ring = "gke-keyring"
-  google_kms_crypto_key_iam_member_crypto_key_id = "projects/sb-izal-20240703-111957/locations/europe-west3/keyRings/gke-keyring/cryptoKeys/gke-key"
+  google_kms_crypto_key_iam_member_crypto_key_id = "gke-key"
   google_kms_crypto_key_iam_member_member = "ServiceAccount:jenkins-gke@sb-izal-20240703-111957.iam.gserviceaccount.com"
   google_storage_bucket_name = "terraform_state_bucket-2247699"
   google_storage_bucket_encryption_default_kms_key_name = "projects/sb-izal-20240703-111957/locations/europe-west3/keyRings/gke-keyring/cryptoKeys/gke-crypto-key"
