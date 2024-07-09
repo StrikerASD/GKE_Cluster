4fdc7b08e2fef92305b6f5b11fc3d922c8aa0bae
izaleski <iwo.zaleski@payback.net>
2024-07-09 15:16:41 +0200

    Updated gke_state_bucket module configuration and applied changes in this repository

--- terraform/bucket.tf
+++ terraform/bucket.tf
@@ -1,15 +1,15 @@
-# module "gke_state_bucket" {
-#   source = "github.com/StrikerASD/GKE_Module_State_Bucket"
-#   google_kms_crypto_key_name = "gke-crypto"
-#   google_kms_key_ring_project = "sb-izal-20240703-111957"
-#   google_kms_crypto_key_iam_member_member = "serviceAccount:jenkins-gke@sb-izal-20240703-111957.iam.gserviceaccount.com"
-#   google_storage_bucket_name = "terraform_state_bucket-2247699"
-# }
-#
-# output "key_ring_id" {
-#   value = module.gke_state_bucket.key_ring
-# }
-#
-# output "key_id" {
-#   value = module.gke_state_bucket.my_key
-# }
+module "gke_state_bucket" {
+  source = "github.com/StrikerASD/GKE_Module_State_Bucket"
+  google_kms_crypto_key_name = "gke-crypto-2"
+  google_kms_key_ring_project = "sb-izal-20240703-111957"
+  google_kms_crypto_key_iam_member_member = "serviceAccount:jenkins-gke@sb-izal-20240703-111957.iam.gserviceaccount.com"
+  google_storage_bucket_name = "terraform_state_bucket-2247699"
+}
+
+output "key_ring_id" {
+  value = module.gke_state_bucket.key_ring
+}
+
+output "key_id" {
+  value = module.gke_state_bucket.my_key
+}
