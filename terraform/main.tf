c8c62fc2105036886aefa879c0d60d82d86d49c2
izaleski <iwo.zaleski@payback.net>
2024-07-10 10:22:55 +0200

    GKE Cluster code modification, deleted service account entries

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,7 @@
 module "gke" {
   source = "github.com/StrikerASD/GKE"
+  sa_account_id = "103483743575851319174"
+  sa_display_name = "jenkins-cluster"
   container_cluster_name = "rancher-cluster"
   container_node_pool_name = "worker-pool"
 }
