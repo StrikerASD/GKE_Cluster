ee3637ed32af7709c37f9f394fc91045207577e5
izaleski <iwo.zaleski@payback.net>
2024-07-10 10:59:31 +0200

    Added VPC network variable for gke module

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,8 +1,9 @@
 module "gke" {
-  source                   = "github.com/StrikerASD/GKE"
-  project                  = "sb-izal-20240703-111957"
-  sa_account_id            = "cluster-service-accoount"
-  sa_display_name          = "cluster service account"
-  container_cluster_name   = "rancher-cluster"
-  container_node_pool_name = "worker-pool"
+  source                      = "github.com/StrikerASD/GKE"
+  project                     = "sb-izal-20240703-111957"
+  sa_account_id               = "cluster-service-accoount"
+  sa_display_name             = "cluster service account"
+  container_cluster_name      = "rancher-cluster"
+  container_node_pool_name    = "worker-pool"
+  google_compute_network_name = "gke-vpc"
 }
