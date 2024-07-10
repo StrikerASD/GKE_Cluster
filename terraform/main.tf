ee7f530a7ff256efd6293be657fdee8d91c32661
izaleski <iwo.zaleski@payback.net>
2024-07-10 10:13:46 +0200

    Updated gke_state_bucket module configuration and applied changes in this repository

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,7 @@
-# module "gke" {
-#   source = "github.com/StrikerASD/GKE"
-#   sa_account_id = "jenkins-gke@sb-izal-20240703-111957.iam.gserviceaccount.com"
-#   sa_display_name = "jenkins-gke"
-#   container_cluster_name = "rancher-cluster"
-#   container_node_pool_name = "worker-pool"
-# }
+module "gke" {
+  source = "github.com/StrikerASD/GKE"
+  sa_account_id = "jenkins-gke@sb-izal-20240703-111957.iam.gserviceaccount.com"
+  sa_display_name = "jenkins-gke"
+  container_cluster_name = "rancher-cluster"
+  container_node_pool_name = "worker-pool"
+}
