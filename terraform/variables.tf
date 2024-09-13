44a7a99897f00e826c78228584a7405e9a7112d5
Iwo <igi31@wp.pl>
2024-09-13 13:11:21 +0200

    Refined the terraform configuration

--- terraform/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "service_account" {
-  type = string
-  default = "serviceAccount:gcp-github-access@sb-izal-20240910-144605.iam.gserviceaccount.com"
-}
-
-variable "google_service_account_account_id" {
-  type    = string
-  default = "service-31545475583@gs-project-accounts.iam.gserviceaccount.com"
-}
