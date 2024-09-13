aa16f9cb6a1e0c687e4521fa7666e40cbaf88c85
Iwo <igi31@wp.pl>
2024-09-13 14:29:31 +0200

    Refined the terraform configuration

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,17 +1,9 @@
-module "gke" {
-  source                      = "github.com/StrikerASD/GKE"
-  project                     = "sb-izal-20240910-144605"
-  sa_account_id               = "cluster-service-account"
-  sa_display_name             = "cluster service account"
-  container_cluster_name      = "rancher-cluster"
-  container_node_pool_name    = "worker-pool"
-  google_compute_network_name = "gke-vpc"
-}
-
-provider "helm" {
-  kubernetes {
-    host                   = google_container_cluster.main.endpoint
-    token                  = data.google_client_config.default.access_token
-    cluster_ca_certificate = base64decode(google_container_cluster.main.master_auth.0.cluster_ca_certificate)
-  }
-}
+# module "gke" {
+#   source                      = "github.com/StrikerASD/GKE"
+#   project                     = "sb-izal-20240910-144605"
+#   sa_account_id               = "cluster-service-account"
+#   sa_display_name             = "cluster service account"
+#   container_cluster_name      = "rancher-cluster"
+#   container_node_pool_name    = "worker-pool"
+#   google_compute_network_name = "gke-vpc"
+# }
