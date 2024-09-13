8d7cbd3766695a82eaaa4ada9c0e86c246fa1a2e
Iwo <igi31@wp.pl>
2024-09-13 11:30:52 +0200

    Changed cloud storage SA to appropriate one and nullified resources that i don't want to be created

--- terraform/bucket.tf
+++ terraform/bucket.tf
@@ -1,7 +1,7 @@
 module "gke_state_bucket" {
   source                                  = "github.com/StrikerASD/GKE_Module_State_Bucket"
-  google_kms_key_ring_name                = null
-  google_kms_crypto_key_name              = null
+  google_kms_key_ring_name                = "gke-keyring-2"
+  google_kms_crypto_key_name              = "gke-crypto-2"
   google_kms_key_ring_project             = "sb-izal-20240910-144605"
   google_kms_crypto_key_iam_member_member = var.service_account
   google_storage_bucket_name              = "terraform_state_bucket-2247699"
