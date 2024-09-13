52708fb589cb510c0a25a51dbde73568b3ac57b0
Iwo <igi31@wp.pl>
2024-09-13 11:03:45 +0200

    Updated the project_id because it was outdated.

--- terraform/bucket.tf
+++ terraform/bucket.tf
@@ -2,8 +2,8 @@ module "gke_state_bucket" {
   source                                  = "github.com/StrikerASD/GKE_Module_State_Bucket"
   google_kms_key_ring_name                = "gke-keyring"
   google_kms_crypto_key_name              = "gke-crypto"
-  google_kms_key_ring_project             = "sb-izal-20240703-111957"
-  google_kms_crypto_key_iam_member_member = "serviceAccount:jenkins-gke@sb-izal-20240703-111957.iam.gserviceaccount.com"
+  google_kms_key_ring_project             = "sb-izal-20240910-144605"
+  google_kms_crypto_key_iam_member_member = "serviceAccount:gcp-github-access@sb-izal-20240910-144605.iam.gserviceaccount.com"
   google_storage_bucket_name              = "terraform_state_bucket-2247699"
 }
 
--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,6 @@
 module "gke" {
   source                      = "github.com/StrikerASD/GKE"
-  project                     = "sb-izal-20240703-111957"
+  project                     = "sb-izal-20240910-144605"
   sa_account_id               = "cluster-service-accoount"
   sa_display_name             = "cluster service account"
   container_cluster_name      = "rancher-cluster"
