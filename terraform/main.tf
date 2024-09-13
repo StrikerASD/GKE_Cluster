ab7cb599b4710b7f36f127419d2a0184fee211c0
Iwo <igi31@wp.pl>
2024-09-13 13:23:27 +0200

    Refined the terraform configuration

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,3 +7,11 @@
 #   container_node_pool_name    = "worker-pool"
 #   google_compute_network_name = "gke-vpc"
 # }
+
+provider "helm" {
+  kubernetes {
+    host                   = google_container_cluster.main.endpoint
+    token                  = data.google_client_config.default.access_token
+    cluster_ca_certificate = base64decode(google_container_cluster.main.master_auth.0.cluster_ca_certificate)
+  }
+}
