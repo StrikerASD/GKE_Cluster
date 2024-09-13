a6e7edbd335e40efd7ff33f738f9f97548c04e49
Iwo <igi31@wp.pl>
2024-09-13 11:25:59 +0200

    Commented gke module, because first i need to create state bucket.

--- terraform/bucket.tf
+++ terraform/bucket.tf
@@ -5,6 +5,7 @@ module "gke_state_bucket" {
   google_kms_key_ring_project             = "sb-izal-20240910-144605"
   google_kms_crypto_key_iam_member_member = var.service_account
   google_storage_bucket_name              = "terraform_state_bucket-2247699"
+  google_service_account_account_id       = var.google_service_account_account_id
 }
 
 output "key_ring_id" {
