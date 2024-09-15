2f46295104c52e229d8a1890680e94731329e07f
root <admin@example.com>
2024-09-15 19:34:06 +0200

    Updated GKE module for deletion_protection argument

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,9 +1,10 @@
-# module "gke" {
-#   source                      = "github.com/StrikerASD/GKE"
-#   project                     = "sb-izal-20240910-144605"
-#   sa_account_id               = "cluster-service-account"
-#   sa_display_name             = "cluster service account"
-#   container_cluster_name      = "rancher-cluster"
-#   container_node_pool_name    = "worker-pool"
-#   google_compute_network_name = "gke-vpc"
-# }
+module "gke" {
+  source                      = "github.com/StrikerASD/GKE"
+  project                     = "sb-izal-20240910-144605"
+  sa_account_id               = "cluster-service-account"
+  sa_display_name             = "cluster service account"
+  container_cluster_name      = "rancher-cluster"
+  container_node_pool_name    = "worker-pool"
+  google_compute_network_name = "gke-vpc"
+  deletion_protection         = false
+}
