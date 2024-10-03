59d34bf038b5a8e0b534812e7945dc2db5f58e7d
Iwo <igi31@wp.pl>
2024-10-03 15:03:09 +0200

    Removing state lock

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -5,8 +5,3 @@ variable "project_id" {
 variable "project_number" {
   type = string
 }
-
-variable "default_compute_service_account" {
-  type = string
-  description = "Service account which is created with the GCP project"
-}
