49b9558f6e283bdae8361714fa882bcffc1b6229
Iwo <igi31@wp.pl>
2024-10-03 15:06:48 +0200

    Removing state lock

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,11 +3,15 @@
 # It's crucial to update it's name here in this module usage.
 #################
 
+data "google_compute_default_service_account" "default" {
+  project = var.project
+}
+
 module "gke" {
   source                      = "github.com/StrikerASD/GKE"
   project                     = var.project_id
-  sa_account_id               = var.default_compute_service_account
-  sa_display_name             = "Default compute service account"
+  sa_account_id               = data.google_compute_default_service_account.default.email
+  sa_display_name             = "cluster service account"
   container_cluster_name      = "rancher-cluster"
   container_node_pool_name    = "worker-pool"
   google_compute_network_name = "gke-vpc"
