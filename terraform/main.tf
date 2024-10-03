560a42e8fe63dada87300affe219f781cbccecb0
Iwo <igi31@wp.pl>
2024-10-03 14:13:13 +0200

    Changes to existing service account

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,12 @@
+### IMPORTANT ###
+# When GCP box is created, new compute service account is created by default.
+# It's crucial to update it's name here in this module usage.
+#################
+
 module "gke" {
   source                      = "github.com/StrikerASD/GKE"
   project                     = var.project_id
-  sa_account_id               = "cluster-service-account"
+  sa_account_id               = "${var.project_number}-compute"
   sa_display_name             = "cluster service account"
   container_cluster_name      = "rancher-cluster"
   container_node_pool_name    = "worker-pool"
